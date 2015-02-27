0a15c629da04c88e14ca8dc3d2069ae2d3ff584c
Alejandro <Alejandromc14@gmail.com>
2015-02-27 01:09:02 -0600

    Added gui and Healt/shield bars

---
+++ IronLegion/Assets/Scripts/GuiManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour {
+	public Health health;
+
+	public InstantGuiSlider healthBar;
+	public InstantGuiSlider shieldBar;
+	// Use this for initialization
+	void Start () {
+		//Set healthBar
+		healthBar.value = 0;
+		healthBar.max = 0;
+		healthBar.min = 0;
+		healthBar.shownValue = 100;
+		//Set ShieldBar
+		shieldBar.value = 0;
+		shieldBar.max = 0;
+		shieldBar.min = 0;
+		shieldBar.shownValue = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shieldBar.shownValue = (health.currentShield / health.maxShield) * 100f;
+		shieldBar.max = 100 - (health.currentShield / health.maxShield) * 100f;
+		healthBar.shownValue = (health.currentHealth / health.maxHealth) * 100f;
+		healthBar.max = 100 - (health.currentHealth / health.maxHealth) * 100f;
+	}
+}
---
+++ IronLegion/Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+		public float maxHealth = 100;
+		public float maxShield = 100;
+		public float currentHealth = 100;
+		public float currentShield = 100;
+		public float regenDelay = 0;
+		public float maxRegenDelay = 5;
+		public float regenRate = 20;
+
+		// Use this for initialization
+		void Start ()
+		{
+				regenDelay = 0;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (regenDelay < maxRegenDelay) {
+						regenDelay += Time.deltaTime;
+				} else {
+						if (currentShield < maxShield) {
+								currentShield += regenRate * Time.deltaTime;
+						}
+				}
+	
+		}
+
+		public void ApplyDamage (float damage)
+		{
+				if (currentShield > 0) {
+						currentShield -= damage;
+				} else {
+						if (currentHealth > 0) {
+								currentHealth -= damage;
+						}
+					
+		
+				}
+		regenDelay = 0;
+		}
+}
