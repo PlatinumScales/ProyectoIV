13cce07edb29f7168f9985005711c7f3f3af189a
Alejandro <Alejandromc14@gmail.com>
2015-02-27 01:20:42 -0600

    Added Splash Screen : lightHouse :+1:

---
+++ IronLegion/Assets/Scripts/DelayScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayScript : MonoBehaviour {
+	public float delayTime = 5;
+	public int nextlevel = 1;
+	IEnumerator Start(){
+		yield return new WaitForSeconds (delayTime);
+		AutoFade.LoadLevel (nextlevel ,1,1,Color.black);
+	}
+}
