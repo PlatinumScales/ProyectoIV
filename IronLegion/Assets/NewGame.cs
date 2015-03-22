e257b8bf5b23ed9065f5ad91a5ae723d14dce228
Usaga128 <estebanusaga@hotmail.com>
2015-03-22 02:32:38 -0600

    Se agrega UI de seleccion de personaje
    Se completa el menuUI y se agrega la seleccion de personaje.

---
+++ IronLegion/Assets/NewGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+	public void NextLevelButton(int pLevel)
+	{
+		
+		
+		AutoFade.LoadLevel(pLevel ,1,1,Color.black);
+	}
+}
---
+++ IronLegion/Assets/QuitLink.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitLink : MonoBehaviour {
+
+	// Use this for initialization
+	public void Quit () {
+		Application.Quit ();
+	}
+	
+ 
+}
