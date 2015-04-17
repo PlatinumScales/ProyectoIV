82b19a9b63311a7fd2774dec39ebd1d7c47fc8c7
Chris Solis <solisalp@gmail.com>
2015-04-17 01:12:27 -0600

    Credits
    Adding end credits scene

---
+++ IronLegion/Assets/Scripts/Credits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public GameObject creditCam;
+	public int speed = 1;
+	public string level;
+
+	// Update is called once per frame
+	void Update () {
+		creditCam.transform.Translate (Vector3.down * Time.deltaTime * speed);
+
+	}
+
+	IEnumerator waitFor(){
+
+		yield return new WaitForSeconds (90);
+		AutoFade.LoadLevel ("LightHouseSplash" ,1,2,Color.gray);
+		//Application.LoadLevel (level);
+		//AutoFade.LoadLevel ("LightHouseSplash" ,1,2,Color.gray);
+
+	}
+}
